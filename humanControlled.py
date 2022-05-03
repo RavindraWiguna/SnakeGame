579af3cabb06cda3ed71f9ff94e8ed048eb9efc3
RavindraWiguna <ravindrawiguna@gmail.com>
2022-05-03 22:41:15 +0700

    separate script for human to play

---
+++ humanControlled.py
@@ -0,0 +1,25 @@
+import pygame
+from snakegame import Game
+
+if __name__ =="__main__":
+    width, height = 480, 480
+    win = pygame.display.set_mode((width, height))
+    snake_game = Game(win, width, height, 24)
+    clock = pygame.time.Clock()
+    isRunning = True
+    while isRunning:
+        clock.tick(20)
+        # snake_game.mini_vision(True)
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                isRunning = False
+                break
+        
+        game_info = snake_game.loop(True, None)
+        isRunning = isRunning and not game_info.game_over
+        snake_game.draw()
+        
+        pygame.display.update()
+    
+    snake_game.stop()
+    pygame.quit()

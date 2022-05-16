ce6aae1be0b5b9855e6a0fcd05772074271ea46a
RavindraWiguna <ravindrawiguna@gmail.com>
2022-05-16 23:12:14 +0700

    fix bug: last move is always default to according input/default

--- snakegame/point.py
+++ snakegame/point.py
@@ -1,12 +1,14 @@
+from snakegame.enums import Direction
 class Point:
-    def __init__(self, x: int, y:int, last_move) -> None:
+    # DIRECTION_STATE = {Direction.UP.value: 'U', Direction.DOWN.value:'D', Direction.LEFT.value:'L', Direction.RIGHT.value:'R', Direction.NaN.value: 'N'}
+    def __init__(self, x: int, y:int, last_move=Direction.NaN) -> None:
         self.x = x
         self.y = y
         # Addition for A*
         self.f = 0
         self.g = 0
         self.h = 0
-        self.last_move = None
+        self.last_move = last_move
     
     def __gt__(self, other):
         if(self.f == other.f):
@@ -17,7 +19,11 @@ class Point:
         return self.x == other.x and self.y == other.y
     
     def __str__(self) -> str:
-        return f'({self.x}, {self.y})'
+        return f'({self.x}, {self.y}) | ({self.g}+{self.h}={self.f}) | {self.last_move.name}'
+    
+    def __repr__(self) -> str:
+        return f'({self.x}, {self.y}) | {self.last_move.name}'
     
     def get_state(self):
-        return f'{self.x}|{self.y}'# to avoid confusion x=1, y=11, or x=11, y=1
+        # print(self.last_move)
+        return f'{self.x}|{self.y}|{self.last_move.name}'# to avoid confusion x=1, y=11, or x=11, y=1
